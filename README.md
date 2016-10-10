ad6212e9d267a9cb39b201c66b88c2e9d59f3abd
Kyle <saxberg@email.arizona.edu>
2016-10-09 23:03:54 -0700

    Having difficulties with R^2, need to make sure regressing correctly

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TModel
 Estimating traffic flow based on application of Gravity Law.
+
+Note: Some difference is shown in the output of this program in comparison 
+	to Matlab implementations of this regression. The difference is 
+	minor, and may be due to algorithmic differences in handling
+	large number inputs. Further verification of the algorithms 
+	should be undertaken. 
--- gravity.py
+++ gravity.py
@@ -1,7 +1,6 @@
 # Runs a basic gravity law analysis, given the population table and distance table
 import numpy as np
-from sklearn import linear_model
-
+import pprint
 def gravity(pop, dist, roadData):
 	gravitys = np.zeros(dist.shape);
 	dim = len(pop.keys());
@@ -13,44 +12,47 @@ def gravity(pop, dist, roadData):
 				indices.append((i,j));
 	# Start with population product in gravity matrix
 	for i,j in indices:
-		gravitys = pop[i][0]*pop[j][0];
+		gravitys[i][j] = pop[i][0]*pop[j][0];
 	# Now run over beta values, divide by distance^beta
 	#  Dumping values into a matrix with beta, K, R^2
-	analysis = np.zeros(20,3)
-	for beta in np.arange(0.1, 2.0, .1):
+	analysis = np.zeros([20,3])
+	for beta in np.arange(0.1, 2.1, .1):
 		i = int(10*beta)
-		gravityBeta = gravitys
+		gravityBeta = np.copy(gravitys)
 		mean = 0
-		for i, j in indices:
-			gravityBeta[i][j] = gravityBeta[i][j] / (distance[i][j]**beta)
-			mean += gravityBeta[i][j]
+		for j, k in indices:
+			gravityBeta[j][k] = gravityBeta[j][k] / (dist[j][k]**beta)
+			mean += roadData[j][k]
 		mean /= len(indices)
-		analysis[i][0] = beta;
-		analysis[i][1] = singleRegression(gravityBeta, roadData, indices);
-		analysis[i][2] = rSquared(analysis[i][1], gravityBeta, roadData, indices, mean);
+		analysis[i-1][0] = beta;
+		analysis[i-1][1] = singleRegression(gravityBeta, roadData, indices);
+		analysis[i-1][2] = rSquared(analysis[i-1][1], roadData, gravityBeta, indices, mean);
 	return analysis
 		
 
 def singleRegression(gravityBeta, roadData, indices):
-	# from scipy import stats
-	x_values = []
-	y_values = []
+	# Solving the linear regression y = mx
+	#   where x = GravityEstimate
+	#         y = RoadData
+	#  This means running lstsq on x\y
+	x_values = np.zeros([len(indices), 1])	
+	y_values = np.zeros([len(indices), 1])
+
+	index = 0
 	for i,j in indices:
-		x_values.append(gravityBeta[i][j])
-		y_values.append(roadData[i][j])
-	#slope, intercept, r_value, p_value, std_err = stats.linregress(x_values, y_values)
-	#print("Slope: %f\nIntercept: %f\nr_value: %f" % [slope, intercept, r_value])
-	model = linear_model.LinearRegression(fit_intercept = False)
-	fit = model.fit(x_values, y_values)
-	return fit.intercept
-	
-def rSquared(k, gravityBeta, roadData, indices, mean):
+		x_values[index] = gravityBeta[i][j]
+		y_values[index] = roadData[i][j]
+		index += 1
+	slope = np.linalg.lstsq(x_values,y_values)[0]
+	return slope
+
+def rSquared(k, roadData, gravityBeta, indices, mean):
 	# for numpy arrays, array*array should define element by element multiplication
 	top = 0
 	bottom = 0
 	for i,j in indices:
-		top += (gravityBeta[i][j] - k*roadData[i][j])**2
-		bottom += (gravityBeta[i][j] - mean)**2
+		top += (k*gravityBeta[i][j]-roadData[i][j])**2
+		bottom += (roadData[i][j] - mean)**2
 	return 1-(top/bottom)
 	
 
@@ -62,4 +64,8 @@ if __name__ == '__main__':
 		dist = parseData.parseDistance(sys.argv[2]);
 		roadData = parseData.parseDistance(sys.argv[3]);
 		analysis = gravity(pop, dist, roadData);
+		print("[Beta, Slope, R^2]")
 		print(analysis)
+		#for i in range(20):
+		#	print("i: ",i)
+		#	pprint.pprint(roadData*analysis[i][1])
