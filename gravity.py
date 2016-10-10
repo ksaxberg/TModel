16147efc89d0c725fed7d1e071cfc5c69eb4fb7e
Kyle <saxberg@email.arizona.edu>
2016-10-10 15:40:24 -0700

    Regressions are close to Matlab regressions. R^2 cannot be applied to a no-intercept linear regression, as R^2 makes an intercept comparison.

--- gravity.py
+++ gravity.py
@@ -1,5 +1,6 @@
 # Runs a basic gravity law analysis, given the population table and distance table
 import numpy as np
+from scipy import stats
 import pprint
 def gravity(pop, dist, roadData):
 	gravitys = np.zeros(dist.shape);
@@ -27,10 +28,10 @@ def gravity(pop, dist, roadData):
 		analysis[i-1][1] = regressionNoIntercept(gravityBeta, travel)
 		predicted = [analysis[i-1][1]*x for x in gravityBeta]
 		analysis[i-1][2] = rSquared(predicted, travel)
-		print("Predicted: ")
-		pprint.pprint(predicted)
-		print("Actual: ")
-		pprint.pprint(travel)
+		#print("Predicted: ")
+		#pprint.pprint(predicted)
+		#print("Actual: ")
+		#pprint.pprint(travel)
 		analysis[i-1][3], analysis[i-1][4] = regressionIntercept(gravityBeta, travel) 
 		predicted = [(analysis[i-1][3]*x+analysis[i-1][4]) for x in gravityBeta]
 		analysis[i-1][5] = rSquared(predicted, travel)
@@ -53,18 +54,22 @@ def regressionIntercept(gravityBeta, travel):
 	y_values = np.array(travel)
 	A = np.vstack([x_values, np.ones(len(x_values))]).T
 	slope, intercept = np.linalg.lstsq(A,y_values)[0]
+	#slope2, intercept2, r_value, p_value, std_err = stats.linregress(gravityBeta, travel)
+	#print("Slope: ", slope2)
+	#print("Intercept: ", intercept2)
+	#print("R^2: ", r_value**2)
 	return [slope, intercept]
-	
 
 def rSquared(predicted, actual):
 	# sum(predicted - actual)^2
 	# divided by sum(predicted - mean)^2
-	SSE = sum([(actual[i] - predicted[i])**2 for i in range(len(predicted))])
+	#SSE = sum([(actual[i] - predicted[i])**2 for i in range(len(predicted))])
 	mean = sum(actual)/len(actual)
 	SSTO = sum([(actual[i] - mean)**2 for i in range(len(predicted))])
 	SSR = sum([(predicted[i]-mean)**2 for i in range(len(predicted))])
 	#return 1-(SSE/SSTO)
 	return SSR/SSTO
+	
 	
 
 if __name__ == '__main__':
