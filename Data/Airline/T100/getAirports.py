331e0967600fb227f3968bd3af202af93049e260
Kyle <saxberg@email.arizona.edu>
2017-04-05 22:45:12 -0700

    Restructured

---
+++ Data/Airline/T100/getAirports.py
@@ -0,0 +1,34 @@
+import sys
+import pprint
+
+"""
+Recovers the list of airports from the T100.txt dataset,
+printing out a list ordered by number of passengers,
+for each airport there is a tuple:
+    [Int(numberOfFlights), Int(PassengersLeaving), Int(PassengersArriving)]
+"""
+
+airports = {} 
+with open(sys.argv[1], 'r') as f:
+    f.readline()
+    for line in f:
+        passengers = float(line.split(',')[0])
+        service = line.split(',')[-2]
+        if service != '"F"' and service != '"L"':
+            continue 
+        first = line.split(',')[3]
+        first = first[1:-1]
+        second = line.split(',')[4]
+        second = second[1:-1]
+        if first not in airports.keys():
+            airports[first] = [0, 0, 0]
+        if second not in airports.keys():
+            airports[second] = [0, 0, 0]
+        airports[first][0] += 1
+        airports[first][1] += passengers
+        airports[second][0] += 1
+        airports[second][2] += passengers
+
+ports = airports.items()
+ports.sort(key=lambda x: x[1])
+pprint.pprint(ports[::-1])
---
+++ Data/Airline/T100withGroupingAndDist/removeSmallPop/RoutesToCombine.py
@@ -0,0 +1,16 @@
+combine = {("LGA", "JFK", "EWA", "HPN", "ISP"): "LGA",
+("IAD", "DCA"): "IAD",
+("MIA", "FLL", "PBI"): "MIA",
+("OAK", "SFO" ): "OAK",
+("ORD", "MDW"): "ORD",
+("DAL", "DFW"): "DAL",
+("HOU", "IAH" ): "HOU",
+("LAX", "SNA", "BUR", "ONT"): "LAX",
+("MCO", "SFB"): "MCO",
+("PHX", "AZA" ): "PHX",
+("PIE", "TPA"): "PIE",
+("PWM", "PDX"): "PWM",
+("CAK", "CLE"): "CAK",
+("ANI", "BET"): "ANI",
+("UNK", "OME"): "UNK"}
+routeList = ["LGA", "JFK", "EWA", "HPN", "ISP", "IAD", "DCA", "MIA", "FLL", "PBI", "OAK", "SFO" ,"ORD", "MDW", "DAL", "DFW", "HOU", "IAH" ,"LAX", "SNA", "BUR", "ONT", "MCO", "SFB", "PHX", "AZA" ,"PIE", "TPA", "PWM", "PDX", "CAK", "CLE", "ANI", "BET", "UNK", "OME"]
